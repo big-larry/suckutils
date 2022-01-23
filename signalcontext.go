2ff2c74e42e763f8c92a0c996095f2d1284fe645
Alex <magicb1985@gmail.com>
2022-01-23 17:46:44 +0500

    SIGTERM

---
+++ signalcontext.go
@@ -0,0 +1,20 @@
+package suckutils
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func CreateContextWithInterruptSignal() (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(context.Background())
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-stop
+		cancel()
+	}()
+	return ctx, cancel
+}
